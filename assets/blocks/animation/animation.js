a74bfb1035ab8e7a05b8ceb98d739d290cec0cac
stasizon <stasizon@gmail.com>
2016-11-20 17:45:38 +0200

    Improve start animation

--- assets/blocks/animation/animation.js
+++ assets/blocks/animation/animation.js
@@ -1,10 +1,23 @@
+var startAnimation = document.createElement('div');
+    startAnimation.id = 'startAnimation';
+    document.getElementById('banner').appendChild(startAnimation);
+
+var backgroundAnimation = document.createElement('div');
+    backgroundAnimation.id = 'backgroundAnimation';
+    startAnimation.appendChild(backgroundAnimation);
+
+var logoAnimation = document.createElement('div');
+    logoAnimation.id = 'logoAnimation';
+    logoAnimation.classList.add('banner__logo');
+    backgroundAnimation.appendChild(logoAnimation);
+
 for (var i = 0; i < images.length; i++) {
 
     var previewImage = document.createElement('img');
     previewImage.src = images[i];
     previewImage.classList.add('preview');
 
-    document.getElementById('startAnimation').appendChild(previewImage);
+    startAnimation.appendChild(previewImage);
 
 }
 
